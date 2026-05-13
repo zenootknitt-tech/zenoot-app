598b113aacbd62d80b96a24e2bf1d28e54b3d513
zenootknitt-tech <zenootknitt@gmail.com>
2026-05-13 07:02:33 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -25,6 +25,9 @@ var ASSETS = [
   './beban-operasional.js',
   'https://fonts.googleapis.com/css2?family=Caveat:wght@400;600;700&display=swap',
   'https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@latest/tabler-icons.min.css',
+  './logo.png',
+  './icon-192.png',
+  './icon-512.png',
   'https://cdn.jsdelivr.net/npm/roughjs@4.6.6/bundled/rough.min.js',
 ];
 
